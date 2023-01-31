383bccd1552a6b445325d417714ffe6cf283ea1d
Anton Giertli <agiertli@redhat.com>
2023-01-31 18:44:27 +0000

    minor refactoring

--- labs/lab3/utils/create-secrets.sh
+++ labs/lab3/utils/create-secrets.sh
@@ -4,3 +4,5 @@ oc process -f artemis-secret.yaml \
 -p TRUSTSTORE_PASSWORD=<TODO> \
 -p BROKER_URL=<TODO> \
 | oc apply -f -
+
+oc create secret generic client-truststore --from-file=client.ts
--- solution/lab3/utils/create-secrets.sh
+++ solution/lab3/utils/create-secrets.sh
@@ -5,3 +5,5 @@ oc process -f artemis-secret.yaml \
 -p TRUSTSTORE_PASSWORD=password1! \
 -p BROKER_URL=amqps://rhte-artemis-one-way-ssl-0-svc.tooling.svc.cluster.local:5673 \
 | oc apply -f -
+
+oc create secret generic client-truststore --from-file=client.ts
