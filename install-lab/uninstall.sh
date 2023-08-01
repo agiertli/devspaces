55374691779c2e4f584df037f402b8fc60b2f677
Anton Giertli <agiertli@redhat.com>
2023-08-01 14:08:59 +0200

    tests

--- install-lab/uninstall.sh
+++ install-lab/uninstall.sh
@@ -1,12 +1,15 @@
 ARGOCD_CR_NAMESPACE=openshift-gitops
 ARGOCD_OPERATOR_NAMESPACE=openshift-operators
 ARGOCD_TOOLING_NAMESPACE=tooling
-oc delete application camelk -n ${ARGOCD_CR_NAMESPACE}
-oc delete appproject rhte-camelk-tooling -n ${ARGOCD_CR_NAMESPACE}
-oc delete appproject default -n ${ARGOCD_CR_NAMESPACE}
+oc delete application argocd-rbac -n ${ARGOCD_CR_NAMESPACE}
+oc delete application devspaces-install -n ${ARGOCD_CR_NAMESPACE}
+oc delete application namespace-config -n ${ARGOCD_CR_NAMESPACE}
+oc delete application devspaces-parent-app -n ${ARGOCD_CR_NAMESPACE}
+oc delete appproject devspaces-project -n ${ARGOCD_CR_NAMESPACE}
 oc delete gitopsservice cluster -n ${ARGOCD_CR_NAMESPACE}
 oc delete argocd openshift-gitops -n ${ARGOCD_CR_NAMESPACE}
-oc delete csv openshift-gitops-operator.v1.6.3 -n ${ARGOCD_OPERATOR_NAMESPACE}
+oc delete csv openshift-gitops-operator.v1.9.1 -n ${ARGOCD_OPERATOR_NAMESPACE}
 oc delete subs openshift-gitops-operator -n ${ARGOCD_OPERATOR_NAMESPACE}
 oc delete project ${ARGOCD_CR_NAMESPACE}
 oc delete project ${ARGOCD_TOOLING_NAMESPACE}
+dsc server:delete --delete-all --delete-namespace -n devspaces
